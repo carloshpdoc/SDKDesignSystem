747a98d5a9ac7821a397843ef950239aa1f63a59
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2024-03-16 23:06:55 -0300

    feat: Add TextStyles parameters and sendParameters functions

--- Texts/Classes/TextStyles.swift
+++ Texts/Classes/TextStyles.swift
@@ -1,7 +1,17 @@
 import Foundation
 
 public struct TextStyles {
-    func testSendTextStyles() -> String {
+    var parameters: String
+
+    public init(parameters: String) {
+        parameters = parameters
+    }
+
+    public func testSendTextStyles() -> String {
         "TextStyles"
+    }
+
+    public func sendParameters() -> String {
+        parameters
     }
 }
