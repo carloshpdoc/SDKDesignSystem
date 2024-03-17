5523bb59703407944246b184de44349da1bab1e3
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2024-03-16 21:04:45 -0300

    refactor: Update TokenDefinitions struct with test property

--- Tokens/Classes/TokenDefinitions.swift
+++ Tokens/Classes/TokenDefinitions.swift
@@ -1,7 +1,13 @@
 import Foundation
 
 public struct TokenDefinitions {
-    func testSendTokens() -> String {
-        "TokenDefinitions"
+    var test: String
+
+    init(test: String) {
+        self.test = test
+    }
+
+    public func testSendTokens() -> String {
+        "Ok: - \(test)"
     }
 }
